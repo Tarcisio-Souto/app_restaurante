0b5e25cfd5842110964728b7e63d998d527a2a93
tarcisio_souto <tss.labsi@gmail.com>
2022-05-22 16:46:09 -0300

    Instalando Vue e Inertia. Subindo componentes.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <layout>
+    <Head title="Welcome" />
+    <div class="row">
+      <div class="col-md-12">
+        <H1>Bem-vindo!</H1>
+        
+      </div>
+    </div>
+  </layout>
+</template>
+
+<script>
+import Layout from "../Layout.vue";
+import { Head } from "@inertiajs/inertia-vue";
+export default {
+  components: {
+    Head,
+    Layout,
+  },
+  props: {
+    user: String,
+  },
+};
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+  </body>
+</html>
